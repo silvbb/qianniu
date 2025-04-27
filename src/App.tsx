442c79f3a0ace888d7198506c23d01fad2220aa2
FiliRon <68350162+silvbb@users.noreply.github.com>
2025-04-27 20:35:55 +0800

    33

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import './App.css'
-import Header from './components/Header'
-import GradientCircle from './components/GradientCircle'
-import Spline from '@splinetool/react-spline';
-import Footer from './components/Footer';
-import SocialLinks from './components/SocialLinks';
-import GoogleAd from './components/GoogleAd';
+import "./App.css";
+import Header from "./components/Header";
+import GradientCircle from "./components/GradientCircle";
+import Spline from "@splinetool/react-spline";
+import Footer from "./components/Footer";
+import SocialLinks from "./components/SocialLinks";
+import GoogleAd from "./components/GoogleAd";
 
 function App() {
   return (
     <>
-      <div className="spline-container">
+      {/* <div className="spline-container">
         <Spline scene="https://prod.spline.design/7xbjlBkVVxdgT4gV/scene.splinecode" />
-      </div>
+      </div> */}
       <GradientCircle />
       <Header />
       <SocialLinks />
       <Footer />
-      <GoogleAd slot="YOUR_AD_SLOT_ID" style={{ display: 'block', marginTop: '20px' }} />
+      <GoogleAd
+        slot="YOUR_AD_SLOT_ID"
+        style={{ display: "block", marginTop: "20px" }}
+      />
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
